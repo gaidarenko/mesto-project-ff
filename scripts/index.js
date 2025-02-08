bd11e6b918ce4e62c42e6a9081c8b58fe1dff29e
Georgiy Gaidarenko <gaydar@atim.ru>
2025-02-08 18:08:01 +0300

    Fill the list of cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,24 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(card, callback) {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', callback);
+
+  return cardElement;
+}
+
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
+
+const placesElement = document.querySelector('.places__list');
+
+initialCards.forEach(function(card) {
+  placesElement.append(createCard(card, deleteCard));
+});
